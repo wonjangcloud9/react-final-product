36c8f940f559ed9ea7353f7035043fcc27110b1d
wonjangcloud9 <venuscwj@naver.com>
2024-03-20 22:55:50 +0900

    swiper skeleton

--- src/screens/home/_components/DisplayBoard.tsx
+++ src/screens/home/_components/DisplayBoard.tsx
@@ -22,6 +22,53 @@ import { IAPIResponse, getPopular, makeBgPath } from "../../../api";
 import { IMovie } from "../../../api";
 import { RightIcon } from "../../../components/right-icon";
 
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const BackgroundImage = styled(motion.img)`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const ContentContainer = styled(motion.div)`
+  position: absolute;
+  top: 25%;
+  left: 20%;
+  transform: translate(-40%, -20%);
+  color: white;
+  text-align: center;
+  width: 25%;
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 1rem;
+  border-radius: 10px;
+`;
+
+const ContentTitle = styled(motion.div)`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const ContentDescription = styled(motion.div)`
+  font-size: 1rem;
+  font-weight: lighter;
+  margin-bottom: 1rem;
+`;
+
+const ContentFowardButton = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  color: rgba(0, 0, 0, 0.8);
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 0.8rem;
+`;
+
 const SWIPER_DELAY = 2.5 * 1000;
 
 export const DisplayBoard = () => {
@@ -41,7 +88,6 @@ export const DisplayBoard = () => {
 
   return (
     <Swiper
-      // install Swiper modules
       modules={[Navigation, Scrollbar, A11y, Autoplay, EffectFade]}
       autoplay={{ delay: SWIPER_DELAY, disableOnInteraction: false }}
       spaceBetween={0}
@@ -61,65 +107,23 @@ export const DisplayBoard = () => {
     >
       {data?.results.slice(1, 6).map((movie: IMovie, index) => (
         <SwiperSlide key={index}>
-          <img
+          <BackgroundImage
             src={makeBgPath(movie.backdrop_path)}
             alt={movie.title}
-            style={{
-              width: "100%",
-              objectFit: "cover",
-            }}
           />
-          <div
-            style={{
-              position: "absolute",
-              top: "25%",
-              left: "20%",
-              transform: "translate(-40%, -20%)",
-              color: "white",
-              textAlign: "center",
-              width: "25%",
-              backgroundColor: "rgba(0, 0, 0, 0.3)",
-              padding: "1rem",
-              borderRadius: "10px",
-            }}
-          >
-            <div
-              style={{
-                fontSize: "1.5rem",
-                fontWeight: "bold",
-                marginBottom: "1rem",
-              }}
-            >
-              {movie.title}
-            </div>
-            <p
-              style={{
-                fontSize: "0.8rem",
-                fontWeight: "lighter",
-                marginBottom: "1rem",
-              }}
-            >
+          <ContentContainer>
+            <ContentTitle>{movie.title}</ContentTitle>
+            <ContentDescription>
               {movie.overview.slice(0, 100) + "..."}
-            </p>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "white",
-                color: "rgba(0,0,0,0.8)",
-                padding: "0.5rem 1rem",
-                border: "none",
-                borderRadius: "5px",
-                cursor: "pointer",
-                fontWeight: "bold",
-                fontSize: "0.8rem",
-              }}
+            </ContentDescription>
+            <ContentFowardButton
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.9 }}
             >
-              <div>보러가기</div>
+              보러가기
               <RightIcon />
-            </div>
-          </div>
+            </ContentFowardButton>
+          </ContentContainer>
         </SwiperSlide>
       ))}
     </Swiper>
@@ -133,8 +137,55 @@ const SwiperSkeleton = () => {
         width: "100%",
         minHeight: "300px",
         maxHeight: "500px",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
+        backgroundColor: "rgba(0, 0, 0, 0.3)",
       }}
-    ></div>
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "10%",
+          left: "20%",
+          transform: "translate(-40%, -20%)",
+          color: "white",
+          textAlign: "center",
+          width: "25%",
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
+          padding: "1rem",
+          borderRadius: "10px",
+        }}
+      >
+        <div
+          style={{
+            marginBottom: "1rem",
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            width: "100%",
+            height: "1.5rem",
+          }}
+        />
+        <div
+          style={{
+            marginBottom: "1rem",
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            width: "100%",
+            height: "1.5rem",
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white",
+            color: "rgba(0, 0, 0, 0.8)",
+            padding: "0.5rem 1rem",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            fontWeight: "bold",
+            fontSize: "0.8rem",
+          }}
+        />
+      </div>
+    </div>
   );
 };
